7521a10054849628458739edd7df74f910528754
lee <lee@pop-os.localdomain leeparker0910@gmail.com>
2022-03-10 17:23:29 +0530

    Add option feature with color change on right option JS script

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,4 +1,4 @@
-$('form').submit(e => {
+$('#mform').submit(e => {
   const fname = $('#mform .search-box').val();
   if (!validation($('#mform .search-box').val(), $('#err'))) {
     e.preventDefault();
@@ -12,6 +12,32 @@ $('form').submit(e => {
     });
   }
 });
+
+$('.op').click(e => {
+  $(this).addClass('correct');
+});
+
+$('#oform').submit(e => {
+  e.preventDefault();
+  if (!validateOptions($('.op input'))) {
+    e.preventDefault();
+    $('#err').text('boxes cannot be empty!!');
+    $('#err').addClass('err');
+  }
+});
+
+const validateOptions = op => {
+  //check if any of the input boxes are empty
+  op.each((index, item) => {
+    let data = '' + $(item).val();
+    console.log(data);
+    if (data === '') {
+      //start fixing the code from , the input returning complete emptyness check with that then procees with backend code
+      return false;
+    }
+  });
+  return true;
+};
 
 const validation = (str, errMsg) => {
   if (str == '') {
